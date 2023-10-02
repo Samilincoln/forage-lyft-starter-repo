4a76f3b426b0da4d935939ce76503b2adbb179ca
Samilincoln <agboolaola150@gmail.com>
2023-10-02 19:54:29 +0100

    creating the models for engine and battery

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,9 +1,12 @@
-
-
-from car import Car
+from battery.battery import Battery
 from datetime import datetime
 
-class NubbinBattery(Car):
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
         if service_threshold_date < datetime.today().date():
--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,7 +1,7 @@
-from car import Car
+from engine.engine import Engine
 
 
-class SternmanEngine(Car):
+class SternmanEngine(Engine):
     def __init__(self, warning_light_is_on):
         self.warning_light_is_on = warning_light_is_on
 
